60583403bf08605879ddf1dc986f88741cf96556
jaehyeon-crema <jaehyeon.kim@cre.ma>
2023-10-31 02:34:25 +0900

    Ch.02 - 옵저버 패턴: 라이브러리에서의 옵저버 패턴(java swing)
    디렉터리 구조 수정
    - 한 chapter 내에서도 여러 예시가 있을 수 있음

---
+++ src/chapter02/weather/concrete/WeatherData.java
@@ -0,0 +1,42 @@
+package concrete;
+
+import java.util.*;
+
+import interfaces.Observer;
+import interfaces.Subject;
+
+public class WeatherData implements Subject {
+    private List<Observer> observers;
+    private float temperature;
+    private float humidity;
+    private float pressure;
+
+    public WeatherData() {
+        this.observers = new ArrayList<Observer>();
+    }
+
+    public void registerObserver(Observer o) {
+        this.observers.add(o);
+    }
+
+    public void removeObserver(Observer o) {
+        this.observers.remove(o);
+    }
+
+    public void notifyObservers() {
+        for(Observer observer : this.observers) {
+            observer.update(temperature, humidity, pressure);
+        }
+    }
+
+    public void measurementsChanged() {
+        this.notifyObservers();
+    }
+
+    public void setMeasurements(float temperature, float humidity, float pressure) {
+        this.temperature = temperature;
+        this.humidity = humidity;
+        this.pressure = pressure;
+        this.measurementsChanged();
+    }
+}
